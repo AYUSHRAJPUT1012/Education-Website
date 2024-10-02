052d10c5103d8f39dd6a086b6d5f4463cddf96b0
Sandeep Vashishtha <sandeepvashishtha@outlook.in>
2024-10-02 15:44:07 +0530

    switches the technology to React

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Courses from './pages/Courses';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+const App = () => (
+  <Router>
+    <Header />
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </main>
+    <Footer />
+  </Router>
+);
+
+export default App;
---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Contact = () => (
+  <div>
+    <h1>Contact Us</h1>
+    <p>Contact form will be here.</p>
+  </div>
+);
+
+export default Contact;
